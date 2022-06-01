962a5776d4ff05eba754f01c3e2ce949a4bd335f
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-06-01 18:21:36 +0200

    added liveness and readiness

--- src/main/java/it/pagopa/devops/springbootshowcase/RootController.java
+++ src/main/java/it/pagopa/devops/springbootshowcase/RootController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/")
@@ -32,11 +34,31 @@ public class RootController {
         return map;
     }
 
+    
     @GetMapping(path = "/envs", produces=MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> envs()
     {
         HashMap<String, String> map = new HashMap<>();
-        map.put("MY_ENV_1", myEnv1);
+        System.getenv().forEach((k, v) -> {
+            map.put(k, v);
+        });
         return map;
+    }
+
+    /*
+     * Liveness & Readiness
+     */
+    @GetMapping(path = "/live", produces=MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public String live()
+    {
+        return "live";
+    }
+
+    @GetMapping(path = "/ready", produces=MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public String ready()
+    {
+        return "ready";
     }
 }
