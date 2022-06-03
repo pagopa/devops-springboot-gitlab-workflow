24f063881033864b5c6fd4e86af3396fedcb6ff7
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2022-06-03 21:54:46 +0200

    feat: Release docker version (#2)
    * test
    
    * added template page with colors
    
    * completed colors logic
    
    * added thanks to mmumshad
    
    * force push
    
    * fix github workflow
    
    * primo
    
    * more tags
    
    * give release plugins

---
+++ src/main/java/it/pagopa/devops/springbootshowcase/AppController.java
@@ -0,0 +1,42 @@
+package it.pagopa.devops.springbootshowcase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+    @Value("${MY_ENV_1:}")
+    private String myEnv1;
+
+    @GetMapping(path = "/", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> root()
+    {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("root", "ok");
+        return map;
+    }
+
+    @GetMapping(path = "/status", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> status()
+    {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "ok");
+        return map;
+    }
+
+    @GetMapping(path = "/envs", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> envs()
+    {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("MY_ENV_1", myEnv1);
+        return map;
+    }
+}
--- src/main/java/it/pagopa/devops/springbootshowcase/RootController.java
+++ src/main/java/it/pagopa/devops/springbootshowcase/RootController.java
@@ -1,64 +1,40 @@
 package it.pagopa.devops.springbootshowcase;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.Random;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class RootController {
 
-    @Value("${MY_ENV_1:}")
-    private String myEnv1;
+    private List<String> colors = Arrays.asList(new String[]{"red","green","blue","blue2","darkblue","pink","lightgoldenrodyellow","black"});
 
-    @GetMapping(path = "/", produces=MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> root()
-    {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("root", "ok");
-        return map;
+    @GetMapping("/")
+    public ModelAndView passParametersWithModelAndView() throws UnknownHostException {
+        ModelAndView modelAndView = new ModelAndView("indexTemplate");
+        modelAndView.addObject("color", getOneColor(""));
+        modelAndView.addObject("hostname", InetAddress.getLocalHost().getHostName());
+        return modelAndView;
     }
 
-    @GetMapping(path = "/status", produces=MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> status()
-    {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("status", "ok");
-        return map;
-    }
 
+    private String getOneColor(String choiseColor){
+        Random r = new Random();
+
+        if (choiseColor != null && choiseColor != ""){
+            return colors.stream()
+                .filter(color -> color.equals(choiseColor))
+                .findAny()
+                .orElse(null);
+        } else {
+            return colors.get(r.nextInt(colors.size()));
+        }
+    }
     
-    @GetMapping(path = "/envs", produces=MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> envs()
-    {
-        HashMap<String, String> map = new HashMap<>();
-        System.getenv().forEach((k, v) -> {
-            map.put(k, v);
-        });
-        return map;
-    }
-
-    /*
-     * Liveness & Readiness
-     */
-    @GetMapping(path = "/live", produces=MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.OK)
-    public String live()
-    {
-        return "live";
-    }
-
-    @GetMapping(path = "/ready", produces=MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.OK)
-    public String ready()
-    {
-        return "ready";
-    }
 }
