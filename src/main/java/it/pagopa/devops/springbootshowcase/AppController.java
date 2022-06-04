ed0fbdf9e79ba6ab2cfddb67fd84c6d0dc50719a
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2022-06-04 21:19:17 +0200

    feat: re-enabled liveness and readyness (#16)

--- src/main/java/it/pagopa/devops/springbootshowcase/AppController.java
+++ src/main/java/it/pagopa/devops/springbootshowcase/AppController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
 
 @RestController
 @RequestMapping("/app")
@@ -30,19 +33,34 @@ public class AppController {
         return map;
     }
 
-    @GetMapping(path = "/status", produces=MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> status()
-    {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("status", "ok");
-        return map;
-    }
-
     @GetMapping(path = "/envs", produces=MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> envs()
     {
         HashMap<String, String> map = new HashMap<>();
-        map.put("MY_ENV_1", myEnv1);
+        System.getenv().forEach((k, v) -> {
+            map.put(k, v);
+        });
+        return map;
+    }
+
+    /*
+     * Liveness & Readiness
+     */
+    @GetMapping(path = "/live", produces=MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, Boolean> live()
+    {
+        HashMap<String, Boolean> map = new HashMap<>();
+        map.put("live", true);
+        return map;
+    }
+
+    @GetMapping(path = "/ready", produces=MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, Boolean> ready()
+    {
+        HashMap<String, Boolean> map = new HashMap<>();
+        map.put("ready", true);
         return map;
     }
 }
