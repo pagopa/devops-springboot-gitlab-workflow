b6a62561bf403fdfa4dbc2e4e1074b879beccb77
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-06-04 22:18:40 +0200

    minor fix

--- src/main/java/it/pagopa/devops/springbootshowcase/AppController.java
+++ src/main/java/it/pagopa/devops/springbootshowcase/AppController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
 
 
 @RestController
