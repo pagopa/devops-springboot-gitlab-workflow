d31dbb7da54c36e6d230a31218298896a9582198
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-06-21 15:47:03 +0200

    fix: Added app get current color in app

--- src/main/java/it/pagopa/devops/springbootshowcase/AppController.java
+++ src/main/java/it/pagopa/devops/springbootshowcase/AppController.java
@@ -3,6 +3,7 @@ package it.pagopa.devops.springbootshowcase;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/app")
 public class AppController {
+
+    @Value("${MY_APP_COLOR:}")
+    private String myAppColor;
 
     @GetMapping(path = "", produces=MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> mainApp()
@@ -34,6 +38,14 @@ public class AppController {
         System.getenv().forEach((k, v) -> {
             map.put(k, v);
         });
+        return map;
+    }
+
+    @GetMapping(path = "/color", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> color()
+    {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("color", myAppColor);
         return map;
     }
 }
