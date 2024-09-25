1a27ee82b4d0932bf77172101fe429163c956723
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-09-25 15:26:33 +0200

    Added startup print message

---
+++ src/main/java/it/pagopa/devops/springbootshowcase/MySystemLogs.java
@@ -0,0 +1,20 @@
+package it.pagopa.devops.springbootshowcase;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.ContextStoppedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MySystemLogs {
+
+    @EventListener
+    public void onStartup(ApplicationReadyEvent event) {
+        System.out.println("🚀 We are ready ----------------------- ✅");
+     }
+
+    @EventListener
+    public void onShutdown(ContextStoppedEvent event) { 
+        System.out.println("⏾ Goodbye");
+     }
+}
