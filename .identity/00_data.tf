88ee4d605c6964ad81d8e002510b1cd3241685a7
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-10-03 14:28:23 +0200

    updated provider for identity

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -3,11 +3,6 @@ data "github_organization_teams" "all" {
   summary_only    = true
 }
 
-data "azurerm_user_assigned_identity" "identity_cd_01" {
-  name                = "${local.prefix}-${var.env_short}-${local.domain}-01-github-cd-identity"
-  resource_group_name = "${local.prefix}-${var.env_short}-identity-rg"
-}
-
 data "azurerm_key_vault" "key_vault" {
   name                = "pagopa-${var.env_short}-kv"
   resource_group_name = "pagopa-${var.env_short}-sec-rg"
@@ -21,20 +16,5 @@ data "azurerm_key_vault" "domain_key_vault" {
 
 data "azurerm_key_vault_secret" "key_vault_sonar" {
   name         = "sonar-token"
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
-
-data "azurerm_key_vault_secret" "key_vault_bot_token" {
-  name         = "bot-token-github"
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
-
-data "azurerm_key_vault_secret" "key_vault_cucumber_token" {
-  name         = "cucumber-token"
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
-
-data "azurerm_key_vault_secret" "key_vault_integration_test_subkey" {
-  name         = "integration-test-subkey"
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
--- .identity/99_main.tf
+++ .identity/99_main.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.30.0"
+      version = "2.50.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.45.0"
+      version = "3.116.0"
     }
     github = {
       source  = "integrations/github"
-      version = "5.18.3"
+      version = "6.3.0"
     }
   }
 
