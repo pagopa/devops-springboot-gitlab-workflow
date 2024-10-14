4ceaea427c714b48e81306c9b77fe0482e9250f2
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2024-10-14 16:16:31 +0200

    feat: Postman branch (#67)
    * added test postman
    
    * Added logs
    
    * Added logs

--- src/main/java/it/pagopa/devops/springbootshowcase/MySystemLogs.java
+++ src/main/java/it/pagopa/devops/springbootshowcase/MySystemLogs.java
@@ -10,7 +10,7 @@ public class MySystemLogs {
 
     @EventListener
     public void onStartup(ApplicationReadyEvent event) {
-        System.out.println("🚀 We are ready ----------------------- ✅✅✅");
+        System.out.println("🚀 We are ready ----------------------- ✅✅✅4️⃣1️⃣");
      }
 
     @EventListener
